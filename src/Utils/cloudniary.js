0e47ecaf1c90026e0973f9165e9f4bda7fbb8f21
afzal-2004 <moa44468@gmail.com>
2024-08-02 21:27:33 +0530

    Add Utils Cloudniary

--- src/Utils/cloudniary.js
+++ src/Utils/cloudniary.js
@@ -7,16 +7,7 @@
     });
     
 
-    // const uploadResult = await cloudinary.uploader
-    // .upload(
-    //     'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-    //         public_id: 'shoes',
-    //     }
-    // )
-    // .catch((error) => {
-    //     console.log(error);
-    // });
-    // console.log(uploadResult);
+   
      const uploadResult= async (localFilePath)=>{
          try {
              if(!localFilePath)
